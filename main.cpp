b1b90060caac36cfcfdc4e4ce58a683542474acf
rylander925 <rylander925@gmail.com>
2025-11-03 12:39:20 -0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+COMSC-210 | Lab 29 | Rylan Der
+IDE Used: Visual studio code
+*/
+
+//Include necessary headers, including for custom data types
+#include <map>
+#include <array>
+#include <list>
+#include "Plant.h"
+#include "Position.h"
+#include "Soil.h"
+
+using namespace std;
+
+//Define function to simulate affects of weather events on nutrient levels for one day
+    //Parameters: map of crops, season
+void WeatherEvent()
