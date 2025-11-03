1bb19b2bee0e504c5895ce567dd30d9fab14b949
rylander925 <rylander925@gmail.com>
2025-11-03 12:51:36 -0800

    outlining code

--- main.cpp
+++ main.cpp
@@ -10,9 +10,27 @@ IDE Used: Visual studio code
 #include "Plant.h"
 #include "Position.h"
 #include "Soil.h"
+#include "Sprinkler.h"
+#include "GardenNode.h"
 
 using namespace std;
 
 //Define function to simulate affects of weather events on nutrient levels for one day
     //Parameters: map of crops, season
-void WeatherEvent()
+
+//Define function to simulate sprinklers
+    //Parameters: map of crops, array of sprinklers, 2D array of garden nodes
+
+//Define function to simulate plant growth
+    //Parameters: map of crops
+
+//Define function to simulate fertilzation event
+
+//Define function to read garden node data
+    //Parameters: map of crops, array of sprinklers, 2D array of garden nodes, filename
+
+//Define main function
+    /*
+        Read garden node data into map of crops, array of sprinklers, and 2D array of garden nodes
+        
+    */
