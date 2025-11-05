4fb87a1419751f021096750243306bcc5c06c9eb
rylander925 <rylander925@gmail.com>
2025-11-05 10:41:25 -0800

    Finished skeleton code for plant class

---
+++ PlantSpecies.h
@@ -0,0 +1,53 @@
+#ifndef PLANTSPECIES_H
+#define PLANTSPECIES_H
+
+#include <string>
+
+//Include details about a partiWcular plant species
+//name, ideal water level, ideal nutrient level, display character
+class PlantSpecies {
+    public:
+        //default constructor
+        Plant();
+
+        //Partial constructor; healthy water range and nurtrient range set to default levels
+        Plant(string name, char displayToken, double idealWater, double idealNutrients);
+
+        //Complete constructor
+        Plant(string name, char displayToken, 
+              double idealWater, double idealNutrients, 
+              double healthyWaterRange, double healthyNutrientRange
+              double tolerableWaterRange, double tolerableNurtientRange);
+
+        //Add standard getters and setters
+        
+        //Updates a growth cycle for a plant based on its current growth and soil health
+        //Increases or decreases growth depending on soil health, and consumes soil health
+        void GrowthCycle(double& growth, Soil& soilInfo);
+
+        //Outputs details about the plant species
+        void Print();
+        
+    private:
+        //name of plant species
+        string name;
+
+        //character to display when printing the 2D garden array
+        char displayToken;
+
+        //water level where plant can grow
+        double idealWater;
+
+        //nutrient level where plant can grow
+        double idealNutrients;
+
+        //Amount above and below water level/nutrient levels that plant can grow
+        double healthyWaterRange;
+        double healthyNutrientRange;
+
+        //Amount above and below water/nutrient levels that plant can survive
+        double tolerableWaterRange;
+        double tolerableNutrientRange;
+}
+
+#endif
