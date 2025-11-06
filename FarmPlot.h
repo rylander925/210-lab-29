6ca1ce4d849c275614d6ba028ed42f0cf2fccbb7
rylander925 <rylander925@gmail.com>
2025-11-05 16:38:50 -0800

    Adding function prototypes

--- FarmPlot.h
+++ FarmPlot.h
@@ -12,16 +12,41 @@ using namespace std;
 class FarmPlot {
     public: 
         //Define function to read garden node data
-            //Parameters: Acts on map of crops, filename
+            //Parameters: filename
+        void ReadData(string filename);
 
         //Define function to output plant information
             //Parameters: None; acts on map of crops
+        void PrintInformation() const;
 
         //Define function to visually output garden as a 2D rectangular plot
             //Parameters: None; acts on map of crops
+        void PrintPlot() const;
 
+        //Define function to change water level associated with all plants, by summing input value
+        //(Negative input value to decrease)
+            //Parameters: double with amount to increase water level
+        void UpdateWater(double waterValue);
+
+        //Define function to change soil health associated with all plants
+        //(Negative input value to decrease)
+            //Parameters: double with amount to increase soil health
+        void UpdateSoil(double fertilizerValue);
+
+        //Define function to change growth level associated with all plants
+        //(Negative input value to decrease)
+            //Parameters: double with amount to increase growth level
+        void UpdateGrowth(double growthValue);
+
+        //Updates plant growth, water, and soil by a 1 day cycle
+        //Will call respective update commands for water, soil, and growth based off weights in PlantSpecies
+        void Update();
+
+        //Enum for readability of the map's value array's list indeces, publicly accessible
+        enum CropData {GROWTH, WATER, SOIL};
     private:
         //Declare map holding plant details
+        //list array holds double of growth, water level, and soil health in that order
         map<PlantSpecies, array<list<double>, 3>> crops;
 
         string name;
